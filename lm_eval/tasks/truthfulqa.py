8aa575431bd7e118d90360d6431a5213341a2114
tliddell13 <tliddell13@gmail.com>
2024-03-22 14:47:44 +0000

    i

--- lm_eval/tasks/truthfulqa.py
+++ lm_eval/tasks/truthfulqa.py
@@ -248,7 +248,7 @@ class TruthfulQAGeneration(Task):
         """
         # TODO: Find a way to cap the number of generated tokens to `50` as in the official implementation.
         # Remove this maybe
-        # completion = rf.greedy_until(ctx, {"until": ["."]})
+        completion = rf.greedy_until(ctx, {"until": ["."]})
         completion = rf.greedy(ctx)
         return completion
 
